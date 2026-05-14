ab238cba9b801cb4af58fbebe4592280b55a5cde
diogocalderaro <diogo@clickmulti.com.br>
2026-05-14 11:58:51 -0300

    fix: adiciona dominios oficiais no CORS

--- server/index.js
+++ server/index.js
@@ -27,7 +27,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:4173'], credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'http://localhost:4173', 
+  'https://amazoniatraveleevents.com',
+  'https://www.amazoniatraveleevents.com',
+  'http://amazoniatraveleevents.com',
+  'http://www.amazoniatraveleevents.com',
+  'https://amazoniatraveleevents-oficial.vercel.app'
+];
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
